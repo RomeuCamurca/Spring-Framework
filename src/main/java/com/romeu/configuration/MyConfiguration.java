38bd3f7538af51cf009050b23e4f29972a582a7e
RomeuCamurca <romeucamurcarabelo@gmail.com>
2021-05-15 15:43:04 -0300

    add project

---
+++ src/main/java/com/romeu/configuration/MyConfiguration.java
@@ -0,0 +1,14 @@
+package com.romeu.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class MyConfiguration {
+
+    @Bean(name = "applicationName")
+    public String applicationName() {
+        return "Sistema de Vendas";
+    }
+
+}
