9b11688fce990b1ab4e93427a20d93aca8377719
romeucamurca <romeucamurcarabelo@gmail.com>
2021-09-09 20:47:49 -0300

    Configuração externalizada

--- src/main/java/com/romeu/VendasApplication.java
+++ src/main/java/com/romeu/VendasApplication.java
@@ -2,9 +2,22 @@ package com.romeu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @SpringBootApplication
+@RestController
 public class VendasApplication {
+
+    @Value("${application.name}")
+    private String applicationName;
+
+    @GetMapping("/hello")
+    public String helloWorld(){
+        return applicationName;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(VendasApplication.class, args);
